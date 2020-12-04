d969ff436554df5f8e29bd4f0ef843ab27da7f4a
GeovaniSouzab080 <75481245+GeovaniSouzab080@users.noreply.github.com>
2020-12-04 09:13:36 -0300

    commit inicial
    subindo o código da calculadora desenvolvida em sala

---
+++ calculadora.js
@@ -0,0 +1,44 @@
+var numero1 = 0
+var operador
+var numero2 = 0
+
+function calc(numero1, operacao, numero2) {
+    switch (operacao) {
+        case "+": return numero1 + numero2
+        case "-": return numero1 - numero2
+        case "*": return numero1 * numero2
+        case "=": return numero1 = numero2
+        case "%": return numero1 % numero2
+    }
+}
+
+function setarnumero(numero) {
+    var divi = document.getElementById("visor").innerText
+    if (!operador) {
+        numero1 = divi + numero
+    } else {
+        numero2 = numero2 + "" + numero
+    }
+    document.getElementById("visor").innerHTML += numero
+}
+
+function setarOperador(operadorDigitado) {
+    operador = operadorDigitado
+    document.getElementById("visor").innerHTML += operadorDigitado
+}
+
+
+function setarResultado() {
+    var resultado
+    if (numero1 && numero2 && operador) {
+        resultado = calc(parseFloat(numero1) , operador, parseFloat(numero2))
+    }
+    else {
+        resultado = 0
+    }
+    document.getElementById("visor").innerHTML=resultado
+    numero1=resultado
+    numero2=0
+    operador=""
+}
+
